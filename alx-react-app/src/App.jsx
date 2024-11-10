21a3d66b1cc98527704e6271dceed8170c45113d
Abdelhamed Tarek <abdelhamedtarek93@gmail.com>
2024-11-10 18:44:27 +0200

    UserProfile Finished

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import WelcomeMessage from "./components/Welcomemessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 import "./App.css";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
   return (
     <>
       <Header />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
