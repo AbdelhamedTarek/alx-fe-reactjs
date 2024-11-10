93f8be6e8dcf8c04594662c871a20e22a2d86b94
Abdelhamed Tarek <abdelhamedtarek93@gmail.com>
2024-11-10 19:34:47 +0200

    Counter Function added

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,15 +6,16 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
+      <Counter />
 
       <MainContent />
 
