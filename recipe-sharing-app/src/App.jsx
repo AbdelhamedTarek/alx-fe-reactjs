37a2298c11b61a60fd49ce433c28451d05346f1b
Abdelhamed Tarek <abdelhamedtarek93@gmail.com>
2024-11-15 15:58:19 +0200

    Recipe App first commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,18 @@
-import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
 import "./App.css";
 
 const App = () => {
   return (
-    <>
-      <RecipeList />
-      <AddRecipeForm />
-    </>
+    <div className="App">
+      <div className="container mx-auto p-6">
+        <h1 className="text-3xl font-semibold text-center mb-6">
+          Recipe Sharing App
+        </h1>
+        <AddRecipeForm />
+        <RecipeList />
+      </div>
+    </div>
   );
 };
 
