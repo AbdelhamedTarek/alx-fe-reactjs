10e6e3dde626f7e6df569a2d9a3a331d73413bed
Abdelhamed Tarek <abdelhamedtarek93@gmail.com>
2024-11-15 16:14:15 +0200

    Recipe App second commit

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,12 +1,14 @@
-// src/components/DeleteRecipeButton.jsx
 import { useRecipeStore } from "./recipeStore";
+import { useNavigate } from "react-router-dom";
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate(); // Hook for navigation
 
   const handleDelete = () => {
     if (window.confirm("Are you sure you want to delete this recipe?")) {
       deleteRecipe(recipeId);
+      navigate("/"); // Navigate back to the recipe list after deletion
     }
   };
 
