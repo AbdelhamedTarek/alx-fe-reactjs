e9fe9da80e875cf66cad6a13483515ebfdc4c714
Abdelhamed Tarek <abdelhamedtarek93@gmail.com>
2024-11-15 15:49:34 +0200

    Recipe App first commit

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  );
+};
+
+export default App;
