c5157d5b5b314d1b6722e5c0488e786c13b46a9d
Abdelhamed Tarek <abdelhamedtarek93@gmail.com>
2024-11-24 22:13:41 +0200

    Formik

--- form-handling-react/vite.config.js
+++ form-handling-react/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    loader: "jsx", // Handle .js files as JSX
+    include: /src\/.*\.(js|jsx)$/, // Apply to .js and .jsx files
+  },
+});
